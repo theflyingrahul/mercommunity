6d0e9a3d4b65db748749a5549997caf56c2c4610
Rahul Pon <theflyingrahul@gmail.com>
2020-04-06 22:32:33 +0530

    Added Azure Deployment Information 1

--- mer/settings.py
+++ mer/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'vpw-72255g7i+9f&0qjmc8r1cnxmona*)z$+&#36d47ml8z)xs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.44.122', '42.111.145.131', 'localhost', '127.0.0.1', 'mercommunity.azurewebsites.net']
+# ALLOWED_HOSTS = ['0.0.0.0', '192.168.44.122', '42.111.145.131', 'localhost', '127.0.0.1', 'mercommunity.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
