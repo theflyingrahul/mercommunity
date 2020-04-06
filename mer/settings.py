fd082e29addb44f2695e2f974156d03f0f907ca4
Rahul Pon <theflyingrahul@gmail.com>
2020-04-07 00:55:59 +0530

    Added Postgres, Removed SQLite3, SQLite committed.

--- mer/settings.py
+++ mer/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'mer.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'mercommunity',
-        # 'USER': 'meradmin',
-        # 'PASSWORD': 'mercommunityadmin',
-        # 'HOST': 'localhost',
-        # 'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mercommunity',
+        'USER': 'meradmin',
+        'PASSWORD': 'mercommunityadmin',
+        'HOST': 'localhost',
+        'PORT': '',
 
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',  # Or path to database file if using sqlite3.
-        'USER': '',  # Not used with sqlite3.
-        'PASSWORD': '',  # Not used with sqlite3.
-        'HOST': '',  # Set to empty string
-        # for localhost. Not used with sqlite3.
-        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+        # 'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': 'sqlite3.db',  # Or path to database file if using sqlite3.
+        # 'USER': '',  # Not used with sqlite3.
+        # 'PASSWORD': '',  # Not used with sqlite3.
+        # 'HOST': '',  # Set to empty string
+        # # for localhost. Not used with sqlite3.
+        # 'PORT': '',  # Set to empty string for default. Not used with sqlite3.
     }
 }
 
