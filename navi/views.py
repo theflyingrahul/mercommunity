d5c2711ed33358bd6555ed429cd81488fa6a643f
Rahul Pon <theflyingrahul@gmail.com>
2020-04-07 22:09:24 +0530

    File upload works

--- navi/views.py
+++ navi/views.py
@@ -1,6 +1,8 @@
+import os
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from navi.models import File as file1
+import datetime
 
 
 # Create your views here.
@@ -14,7 +16,11 @@ def privilege(request):
 def merlan(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['uploaded_file']
+        print(uploaded_file.size)
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
+        ext = os.path.splitext(uploaded_file.name)[1].upper().lstrip('.')
+        file_object = file1(name=uploaded_file.name[0:48], fmt=ext, uploaded_by="rah", file_size=int(uploaded_file.size/1024/1024), date_time=datetime.datetime.now(), url="abc.txt")
+        file_object.save()
     file_query = file1.objects.all()
     return render(request, "navi/merlan.html", {'files': file_query})
